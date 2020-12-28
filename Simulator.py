20f07122c572dc50e4f607dff0f3de85445062b1
Kaushik Patil <64463136+kaushikpatil2911@users.noreply.github.com>
2020-12-28 03:12:05 -0500

    Add files via upload

---
+++ Simulator.py
@@ -0,0 +1,69 @@
+def get_new_path (path, where):
+    if not where:
+        return path 
+        
+    if where[0] == '.':
+        where = where[1:]
+        if not where:
+            return path 
+        where = where[1:]   # remove the slash
+        return get_new_path (path, where)
+    
+    if where[0] == '..':
+        where = where [1:]
+        if not path:
+            return path
+        path.pop()
+        path.pop()
+        if not where:
+            return path 
+        where = where[1:]    # remove the slash
+        return get_new_path (path, where)
+
+    if not where[0].isalnum():
+        return where[0] + ": No such file or directory"
+        
+    if path [-1] != '/':
+        path.append ('/')
+    
+    path.append (where[0])
+    where = where[1:]
+    if not where:
+        return path 
+    where = where[1:]    # remove the slash
+    return get_new_path (path, where)
+    
+
+while True:
+    print ("# ", end = "")
+    command = input().split()
+    if len(command) != 3:
+        print ("Bad input")
+        continue
+    if command[0] != 'mycd':
+        print ("Bad input")
+        continue
+    
+    path = command[1]
+    where = command[2]
+    while path.find("//") != -1:
+        path = path.replace ("//", "/")
+        print (path)
+    while where.find("//") != -1:
+        where = where.replace ("//", "/")
+
+    path = path.replace ("/", "@/@").split("@")
+    path = [item for item in path if item != '']
+    where = where.replace ("/", "@/@").split("@")
+    where = [item for item in where if item != '']
+
+    if where[0] == '/':    # ignore current path, where has the absolute destination path
+        path = ''.join(where)
+        print (path)
+        continue
+    
+    path = get_new_path (path, where)
+    if not path:
+        path = ['/']
+    path = ''.join(path)
+    print (path)
